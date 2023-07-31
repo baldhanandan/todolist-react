26322e092e2eb854619d7af402eb9e835106edad
Nandan <nandan@Nandans-MacBook-Air.local>
2023-08-01 01:44:24 +0530

    revert

--- src/myComponents/Footer.js
+++ src/myComponents/Footer.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
 export const Footer = () => {
-    let foorterStyle={
-        position:"relative",
-        top:"100vh",
-        width:"100%"
-
-    }
+    
   return (
-    <footer className='bg-dark text-light' style={foorterStyle}>
+    <footer className='bg-dark text-light' >
         <p className="text-center">
             Copyright &copy; MyTodolist.com 
         </p>
--- src/myComponents/Todos.js
+++ src/myComponents/Todos.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { TodoIteam } from './TodoIteam'
 
 export const Todos = (props) => {
+    const myStyle={
+        minHeight:"70vh",
+        margin:"10px auto"
+    }
   return (
-    <div className='container'>
-        <h3 className='text-center'>Todos List</h3>
+    <div className='container my-3' style={myStyle}>
+        <h3 className='my-3'>Todos List</h3>
         {props.todos.length===0? "no to do list" : 
          props.todos.map((todo)=>{
             return <TodoIteam todo={todo} key={todo.sno} onDelete={props.onDelete}/>
